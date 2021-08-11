f887ad049146948747258feb0f361ea8be8782f2
李光浩 <liguanghao@viatomtech.com>
2021-08-11 15:58:14 +0800

    fuck 成功了了

--- app/src/main/java/com/vaca/tcp/MainActivity.kt
+++ app/src/main/java/com/vaca/tcp/MainActivity.kt
@@ -1,5 +1,7 @@
 package com.vaca.tcp
 
+import android.content.Intent
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
@@ -7,5 +9,10 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            startForegroundService(Intent(this, TcpClientService::class.java))
+        } else {
+            startService(Intent(this, TcpClientService::class.java))
+        }
     }
 }
